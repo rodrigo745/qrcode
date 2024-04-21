640de32d0d331d2613722817bcf1ce190c440891
rodrigo745 <roodrigoquinteros@gmail.com>
2024-04-21 20:22:57 -0300

    campos vacios

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,13 +7,19 @@ import logoQrCode from "./resource/logo-qr-generator.svg";
 
 export default function Home() {
 
-  const [ texto, setTexto ] = useState();
+  const [ texto, setTexto ] = useState("");
   const [ src, setSrc ] = useState("");
   const [ mostrar, setMostrar ] = useState(false);
-
+  const [ error, setError ] = useState(false);
+  
   const generarQR = ()=>{
-    QRCode.toDataURL(`${texto}`).then(setSrc);
-    mostrar ? setMostrar(false) : setMostrar(true);
+    if(texto.length > 1){
+       QRCode.toDataURL(`${texto}`).then(setSrc);
+       mostrar ? setMostrar(false) : setMostrar(true);
+       setError(false);
+    } else{
+      setError(true);
+    }
   }
   const mostrarCodigo = ()=>{
     mostrar ? setMostrar(false) : setMostrar(true);
@@ -44,6 +50,9 @@ export default function Home() {
             onChange={(e)=> setTexto(e.target.value)}/>
           <button className="ml-0 md:ml-4 text-white rounded-md azulMedioBg px-2 md:px-6 py-2 md:py-3 cursor-pointer" onClick={generarQR}>Generar</button>
           </div>
+          {
+            error && <p className="text-white text-center mt-4">El campo no puede estar vacío</p>
+          }    
         </div>
         
       }
